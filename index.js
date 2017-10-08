aea0cdd575aab84e2d6873262f96af9e6b55b91f
frozinoer <fabrice.rozinoer@gmail.com>
2017-10-09 01:42:07 +0200

    introducing final API and canvas-prebuilt

--- index.js
+++ index.js
@@ -18,6 +18,25 @@ const dotenvResult = require('dotenv').config();
   throw dotenvResult.error
 }*/
 
+//const { createCanvas, loadImage } = require('canvas-prebuilt');
+const Canvas = require('canvas-prebuilt');
+const canvas = new Canvas(640, 335);
+const ctx = canvas.getContext('2d');
+
+// Write "Awesome!"
+ctx.font = '30px Impact'
+ctx.rotate(0.1)
+ctx.fillText('Awesome!', 50, 100)
+
+// Draw line under text
+var text = ctx.measureText('Awesome!')
+ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+ctx.beginPath()
+ctx.lineTo(50, 102)
+ctx.lineTo(50 + text.width, 102)
+ctx.stroke();
+
+console.log('<img src="' + canvas.toDataURL() + '" />');
 
 
 app.set('port', process.env.PORT || 3000);
---
+++ public/transaction.js
@@ -0,0 +1,47 @@
+transaction = {
+  "id": "1b3766b4cac9fc151e31684e",
+  "reason": "",
+  },
+  "owner": {
+    "profile_pic":
+      "https://scontent.xx.fbcdn.net/v/t31.0-1/1965637_10154180079707018_4777005882926731672_o.jpg?oh=1695580fc7a19daef379d1fbccc8ca2d&oe=5A808824",
+    "last_name": "Lavigne",
+    "first_name": "Richard",
+    "id": "59d62f8f0e2b3eac00e61d9f"
+  },
+  "from": {
+    "profile_pic":
+      "https://scontent.xx.fbcdn.net/v/t31.0-1/1965637_10154180079707018_4777005882926731672_o.jpg?oh=1695580fc7a19daef379d1fbccc8ca2d&oe=5A808824",
+    "last_name": "Lavigne",
+    "first_name": "Richard",
+    "id": "59d62f8f0e2b3eac00e61d9f"
+  },
+  "status": "active",
+  "amount": 258,
+  "to": [
+    {
+      "user": {
+        "profile_pic":
+          "https://scontent.xx.fbcdn.net/v/t1.0-1/22154494_383287835436010_1500142750480877589_n.jpg?oh=13860d4f1f304dcc386113f3b7296da3&oe=5A4B9399",
+        "last_name": "Levy",
+        "first_name": "Cédric",
+        "id": "59d62c820e2b3eac00e5f401"
+      },
+      "amount": 129,
+      "parts": 1,
+      "_id": "59d6599199b7abb0d4b31890"
+    },
+    {
+      "user": {
+        "profile_pic":
+          "https://scontent.xx.fbcdn.net/v/t1.0-1/11800302_10153094813967843_4588750750506311854_n.jpg?oh=d6c1a22acd978ee732cd70a1fd45ac5f&oe=5A447B3C",
+        "last_name": "Rozinoer",
+        "first_name": "Fabrice",
+        "id": "59d62ca30e2b3eac00e5f5d9"
+      },
+      "amount": 129,
+      "parts": 1,
+      "_id": "59d6599199b7abb0d4b3188f"
+    }
+  ]
+}
